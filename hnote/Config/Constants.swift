8fe3d42bec1b361712005c7d0271d02e2779c725
Luciano <lpuzer@yahoo.com.br>
2024-03-12 20:54:37 -0300

    add dev and prod environment

---
+++ hnote/Config/Constants.swift
@@ -0,0 +1,8 @@
+//Created by Halbus Development
+
+import Foundation
+
+struct Constants {
+    static let pantry = "PantryItems"
+    static let pantryCategory = "PantryCategory"
+}
--- hnoteApp.swift
+++ hnoteApp.swift
@@ -2,14 +2,21 @@
 
 import SwiftUI
 import SwiftData
-import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
+
 struct hnoteApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+   
     var body: some Scene {
         WindowGroup {
             ContentView()
